4430a8dbfcca185c7b31b50dcc5f3e0db97d6996
Evan Jensen <jensen55@uw.edu>
2021-07-05 22:08:37 -0700

    concurrency

--- concurrency.go
+++ concurrency.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func concurExample() {
 	i := 0
-	for {
+	for i < 10 {
 		fmt.Println(i)
 		i++
 		time.Sleep(time.Millisecond * 500)
 	}
 }
 
+func concurPass(s string) {
+	i := 0
+	for i < 10 {
+		fmt.Println(i, s)
+		i++
+		time.Sleep(time.Millisecond * 500)
+	}
+}
 func callConcurExample() {
-	concurExample()
-	concurExample()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go concurPass("Dog")
+	go concurPass("Bird")
+	go func() {
+		concurExample()
+		wg.Done()
+	}()
+	wg.Wait()
+
+	time.Sleep(time.Microsecond * 10000)
 }
